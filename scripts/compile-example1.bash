7ea1554d9c8601c76f1ac963a39d43e4d9f9615e
Sohail Siadat <sohale@gmail.com>
2024-04-24 15:43:19 +0000

    (draft) make compile script, better than CMake

--- scripts/compile-example1.bash
+++ scripts/compile-example1.bash
@@ -1,18 +1,33 @@
 #!/bin/bash
 set -exu
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+REPO_ROOT=$(git rev-parse --show-toplevel)
+cd $REPO_ROOT
+
+
+# better than CMake
 
 FILE_BASENAME=test_ifcsdk_compiletion
 
+BIN_OUTPUT=$REPO_ROOT/out
+SRC_INPUT=$REPO_ROOT/src
+INPUT_INCLUDES=$REPO_ROOT/includes-symb
+mkdir -p $BIN_OUTPUT
+mkdir -p $INPUT_INCLUDES
+
 # Backup
-mv -f  $FILE_BASENAME.out $FILE_BASENAME.out.old || :  # Don't err
+mv -f  "$BIN_OUTPUT/$FILE_BASENAME.out"  "$BIN_OUTPUT/$FILE_BASENAME.out.old" || :  # Don't err
 
 # includes
-ln -s 
+# /IFC4
+ln -s "$REPO_ROOT/external/oda-ifc-sdk"  $INPUT_INCLUDES/ifcsdk
+
 # Compile
 
 clang++  \
    -std=c++20  -stdlib=libstdc++  \
    ../includes-symb   \
-   $FILE_BASENAME.cpp   \
-  -o $FILE_BASENAME.out
+   $SRC_INPUT/$FILE_BASENAME.cpp   \
+  -o $BIN_OUTPUT/$FILE_BASENAME.out
 #  -v  -lstdc++
