bc54861c3bff61089a8c74da0549d7a3a478272b
Sohail Siadat <sohale@gmail.com>
2024-04-24 15:38:40 +0000

    expanding build script for include s

--- scripts/compile-example1.bash
+++ scripts/compile-example1.bash
@@ -3,8 +3,16 @@ set -exu
 
 FILE_BASENAME=test_ifcsdk_compiletion
 
+# Backup
 mv -f  $FILE_BASENAME.out $FILE_BASENAME.out.old || :  # Don't err
 
-clang++  -std=c++20  -stdlib=libstdc++  $FILE_BASENAME.cpp   \
+# includes
+ln -s 
+# Compile
+
+clang++  \
+   -std=c++20  -stdlib=libstdc++  \
+   ../includes-symb   \
+   $FILE_BASENAME.cpp   \
   -o $FILE_BASENAME.out
 #  -v  -lstdc++
--- src/test_ifcsdk_compiletion.cpp
+++ src/test_ifcsdk_compiletion.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+// symbolic linked using `ln -s`
+#include <oda-ifc-sdk/...>
+
 int main(int argc, char* argv[]) {
    std::cout << "Hello" << std::endl;
    return 0;
