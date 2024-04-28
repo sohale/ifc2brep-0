eca33efe174627d4d5a8aa33d1becd15de06a9e5
Sohail Siadat <sohale@gmail.com>
2024-04-28 13:49:45 +0000

    cmd.exe bash file to run windows commandline in Wine in Linux

---
+++ scripts/cmd_exe.bash
@@ -0,0 +1,24 @@
+set -eux
+
+# see scripts/setup-msvc.bash
+function gitrepo_reset_to_root() {
+   # Sets directory context to the root of the git repository
+   # git rev-parse --show-toplevel
+   SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+   cd $SCRIPT_DIR
+   REPO_ROOT=$(git rev-parse --show-toplevel)
+   cd $REPO_ROOT
+}
+
+
+gitrepo_reset_to_root
+
+mkdir -p $REPO_ROOT/external-tools/wine64
+export WINE64_PREFIX=$REPO_ROOT/external-tools/wine64
+
+echo "Wine folder is: $WINE64_PREFIX"
+
+ls -1 $WINE64_PREFIX >/dev/null  # verify it exists
+
+DISPLAY=:0.0 WINEPREFIX=$WINE64_PREFIX WINARCH=win64  wine64  cmd
+# cd $REPO_ROOT/external-tools/
