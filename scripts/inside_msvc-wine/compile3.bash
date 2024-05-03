d4b791be7283044cfe830e6cdda8920e2df257f1
Sohail Siadat <sohale@gmail.com>
2024-05-03 22:50:32 +0000

    .lib s

--- scripts/inside_msvc-wine/compile3.bash
+++ scripts/inside_msvc-wine/compile3.bash
@@ -49,6 +49,21 @@ define_macros=(
 
 )
 
+libraries_to_link=(
+   IfcCore.lib
+   sdai.lib
+   TD_Gi.lib
+   TD_Ge.lib
+   TD_Root.lib
+   TD_DbRoot.lib
+   TD_DbRoot.lib
+   TD_ExamplesCommon.lib
+   TD_Alloc.lib
+
+   # IfcCore.lib IfcBrepModeler.lib IfcFacetModeler.lib TB_Common.lib
+)
+
+
 #########
 
 compileflags_incl_list=""
@@ -60,6 +75,12 @@ compileflags_definemacros=""
 for def in "${define_macros[@]}"; do
     compileflags_definemacros+=" /D$def"
 done
+
+compileflags_libs=""
+for lib1 in "${libraries_to_link[@]}"; do
+    compileflags_libs+=" $lib1"
+done
+
 
 echo "********************"
 echo $compileflags_definemacros
@@ -77,7 +98,7 @@ echo "********************"
    -Fe./out/a3.exe  \
    \
    /link /LIBPATH:$LIB_PREFIX/vc16_amd64dll \
-   IfcCore.lib IfcBrepModeler.lib IfcFacetModeler.lib TB_Common.lib
+   $compileflags_libs
 
 
 ls -alth ./out
