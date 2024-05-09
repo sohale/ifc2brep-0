ba1dc479671f60d14b000c1b57b112a32eede698
Sohail Siadat <sohale@gmail.com>
2024-05-09 16:13:26 +0000

    execusion (in sol2) script works

--- scripts/wine_run_built_exe_sol2.bash
+++ scripts/wine_run_built_exe_sol2.bash
@@ -4,8 +4,10 @@ set -eux
 # based on: scripts/wine_cmd.exe.bash
 
 echo "This script runs exe (compiled using sol3 env) in the "sol2" env"
-echo "Sol2Env: Wine folder is: $WINE_PREFIX_"
 
+
+# framework's common envs
+source env_sol3.env
 
 # source env1_sol2.env
 # Contains the "sol2"'s env for running wine64
@@ -13,33 +15,31 @@ source env2_sol2.env
 # Contains the built executable and its reauquired PATH
 source built_s23_a3.env
 env
-ls -1 $WINE_PREFIX_ >/dev/null  # verify it exists
+ls -1 $WINEPREFIX >/dev/null  # verify it exists
 ls -1 $BUILT_FILENAME >/dev/null  # verify it exists
 
 
 
-gitrepo_reset_to_root || default_wine_64
+gitrepo_reset_to_root
 
-
-echo "Wine folder is: $WINE_PREFIX_    for  $WINE_COMMAND_"
+# sol2
+echo "Sol2Env: Wine folder is: $WINEPREFIX"
+echo "Wine folder is: $WINEPREFIX    for  $WINE_COMMAND_ using arch:$WINEARCH"
 echo "X-Windows \$DISPLAY=$DISPLAY"
-echo "ODA DLLs are in: $DLL_PREFIX_WINE"
+# sol3 (from)
+echo "ODA DLLs are in: $DLL_PATH"
 echo "executable in: $BUILT_FILENAME"
 
 
 
-
+# DLL_PATH
 # DLL_PREFIX_WINE="z:\\home\\ephemssss\\novorender\\oda-sdk\\vc16\\exe\\vc16_amd64dll"
 # BUILDOUTPUT="./build-output"
 
-WINEPATH="$DLL_PREFIX_WINE;${WINEPATH:-}" wine64 ./build-output/a3.exe^C
+# WINEPATH="$DLL_PATH;${WINEPATH:-}" wine64 ./build-output/a3.exe
 
+echo $WINEARCH $WINEPREFIX $DISPLAY $WINE_COMMAND_
+echo $DLL_PATH $BUILT_FILENAME
 
-
-WINEARCH
-WINEPREFIX
-DISPLAY
-WINE_COMMAND_
-
-export WINEPATH="$DLL_PREFIX_WINE;${WINEPATH:-}"
-$WINE_COMMAND_
+export WINEPATH="$DLL_PATH;${WINEPATH:-}"
+$WINE_COMMAND_  $BUILT_FILENAME
